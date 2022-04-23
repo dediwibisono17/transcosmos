d325e8e7332cb37d778938eeab13f19c17b5d370
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2022-04-23 15:43:18 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,17 +1,3 @@
-new WOW().init();
-// alert(1)
-$(window).scroll(function() {
-    var nav = $('.bg-scroll');
-    var top = 20;
-    if ($(window).scrollTop() >= top) {
-        nav.addClass('bg-black');
-
-    } else {
-        // nav.css('top', '0px')
-        nav.removeClass('bg-black');
-    }
-});
-
 $(".youtube__img").click(() => {
     $(".youtube__img").hide();
     $(".youtube__real").css('display', 'block')
